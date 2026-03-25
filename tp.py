efa5a67b2e5a31cb88cb8326c6f45ef1924377d7
Othmane Dounit <166843345+otm-dnt@users.noreply.github.com>
2026-03-25 12:17:00 +0100

    Create tp.py

---
+++ tp.py
@@ -0,0 +1,19 @@
+# tp.py
+
+class Student:
+    def __init__(self, name, m1, m2, m3):
+        self.name = name
+        self.m1 = m1
+        self.m2 = m2
+        self.m3 = m3
+
+    def average(self):
+        return (self.m1 + self.m2 + self.m3) / 3
+
+
+class SchoolClass:
+    def __init__(self):
+        self.students = []
+
+    def add_student(self, student):
+        self.students.append(student)
